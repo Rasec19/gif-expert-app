2bb51b001cd9a55412a3e84823a7a975dd4c7c82
Rasec Camacho <rasec.camacho19@gmail.com>
2022-12-07 12:12:35 -0700

    Update GiftExpertApp.jsx

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 
 export const GiftExpertApp = () => {
+
+  const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ])
+
+  const onAddCategory = () => {
+    setCategories( 'a' );
+  };
+  
   return (
     <>
+      {/** titulo */}
       <h1>GiftExpertApp</h1>
+
+      {/** Input */}
+
+      {/** Listado de Gif */}
+      <button onClick={  onAddCategory } >Agregar</button>
+      <ol>
+        { 
+        categories.map( category => {
+          return <li key={ category }>{ category }</li>
+        }) 
+        }
+        {/* <li>ABC</li> */}
+      </ol>
+        {/** Gif Item */}
     </>
   );
 }
