2ac456fec8abb374d0a48642b7764195a4cd3fb6
Rasec Camacho <rasec.camacho19@gmail.com>
2023-01-10 16:42:57 -0700

    update

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async ( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=J1So1Itq2u2HgEYdDB5VTe90fUx4rcsM&q=${ category }&limit=20`
+    const res = await fetch( url );
+    const { data } = await res.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url,
+    }));
+
+    console.log(gifs);
+    return gifs;
+};
