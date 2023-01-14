2f4d1cf13511b470a748bc3f1e4dd864b0c0bd9b
Rasec Camacho <rasec.camacho19@gmail.com>
2023-01-14 16:26:42 -0700

    UPDATE

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
 
-export const GifItem = ({ title, url }) => {
-    console.log({title,url})
+
+export const GifItem = ({ title, url, id }) => {
+
   return (
-    <div>GifItem</div>
+    <div className="card">
+        <img src={ url } alt={ title } />
+        <p>{ title }</p>
+    </div>
   )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async ( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=J1So1Itq2u2HgEYdDB5VTe90fUx4rcsM&q=${ category }&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=J1So1Itq2u2HgEYdDB5VTe90fUx4rcsM&q=${ category }&limit=10`
     const res = await fetch( url );
     const { data } = await res.json();
 
