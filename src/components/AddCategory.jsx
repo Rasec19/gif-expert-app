535e44980c853972c259d5c9e8c5017fb13b503f
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-02 16:14:41 -0700

    test

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types'
 
 export const AddCategory = ( { onNewCategory } ) => {
 
@@ -15,12 +16,12 @@ export const AddCategory = ( { onNewCategory } ) => {
         if ( inputValue.trim().length <= 1 ) return;
 
        
-        onNewCategory( inputValue.trim() );
         setinputValue('');
+        onNewCategory( inputValue.trim() );
     };
 
     return (
-        <form onSubmit={ onSubmit }>
+        <form onSubmit={ onSubmit } aria-label="form">
             <input 
                 type="text"
                 placeholder="Buscar gifs"
@@ -30,3 +31,7 @@ export const AddCategory = ( { onNewCategory } ) => {
         </form>
     );
 }
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
+};
