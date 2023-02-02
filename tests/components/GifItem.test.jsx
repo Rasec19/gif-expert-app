96feac40b0ae8923658b23a5968c7fc7540aa616
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-01 17:42:15 -0700

    test Gifitem

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react' 
+import { getByRole, render, screen } from '@testing-library/react' 
 import { GifItem } from '../../src/components/GifItem'
 
 describe('Pruebas en <GifItem />', () => {
@@ -12,6 +12,22 @@ describe('Pruebas en <GifItem />', () => {
 
         expect( container ).toMatchSnapshot();
     });
+
+    test('Debe mostrar la imagen con el URL y el ALT indicado', () => { 
+
+        render( <GifItem title={ title } url={ url } />);
+        // screen.debug();
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+
+    });
+
+    test('Debe de mostrar el titulo enm el componente', () => {
+
+        render( <GifItem title={ title } url={ url } /> );
+        expect( screen.getByText( title ) ).toBeTruthy();
+     })
     
   
 });
